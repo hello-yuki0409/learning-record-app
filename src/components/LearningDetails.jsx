a3d1d9832c3937425a7a8d86d88eb1161e075126
hello-yuki0409 <bobu2784@gmail.com>
2025-07-06 17:40:57 +0900

    産まれてから死ぬまで勉強

---
+++ src/components/LearningDetails.jsx
@@ -0,0 +1,23 @@
+export const LearningDetails = (props) => {
+  const { records, time, error, totalStudyTime, currentGoal, baseGoal } = props;
+  return (
+    <div>
+      <h1>学習記録一覧</h1>
+      <div>
+        <p>入力されている学習内容: {records}</p>
+        <p>入力されている時間: {time} 時間</p>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        <p>
+          合計学習時間：{totalStudyTime} / {currentGoal} 時間
+        </p>
+        {totalStudyTime >= baseGoal && (
+          <p>
+            <span style={{ color: "green", marginLeft: 10 }}>
+              よし！次の目標は {currentGoal} 時間だ🔥 (定量目標)
+            </span>
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
